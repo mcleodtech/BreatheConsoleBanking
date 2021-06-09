8cfaf787ad80699ebe0f2a5f7eb7b6fefe5e6d06
Rob <daringknight@hotmail.com>
2021-06-09 18:53:41 -0500

    First edition

---
+++ BreatheConsoleBanking/BankAccount.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreatheConsoleBanking
+{
+    class BankAccount
+    {
+        //CTL + G to jump to a line number
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private static int accountNumberSeed = 1234567890;
+
+        public List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            //A constructor can be thought of like a form, "this is what
+            //a bank account should look like
+            Owner = name;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of Withdrawl must be positive ");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawl");
+                //The throw will stop the function if the criteria are metS
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
+
+        }
+
+        public string GetAccountHistory()
+        {
+            //Use a string builder to make complex strings
+            var report = new StringBuilder();
+
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                //Making rows with the \t "tab" character
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ BreatheConsoleBanking/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BreatheConsoleBanking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Robert", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawl(294, DateTime.Now, "Desk");
+            Console.WriteLine($"You purchased a {""} for {294} on {DateTime.Now}, your current balance is {account.Balance}");
+
+            account.MakeWithdrawl(593, DateTime.Now, "Window Repair");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+            //Test for a negative balance
+            try
+            {
+                account.MakeWithdrawl(150, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+
+            //Test that the initial balance must be positive:
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+
+    }
+}
